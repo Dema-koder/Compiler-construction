248cfdbe7740975b14244d7485d39583989f8df2
Dema-koder <asotruzikovic@gmail.com>
2024-11-22 20:59:03 +0300

    ComplexProgram.j

--- src/main/java/bytecode/BytecodeGenerator.java
+++ src/main/java/bytecode/BytecodeGenerator.java
@@ -2,61 +2,94 @@ package bytecode;
 
 import ast.ASTNode;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
 public class BytecodeGenerator {
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+
     private final StringBuilder bytecode;
+
+    private String className;
 
     public BytecodeGenerator() {
         this.bytecode = new StringBuilder();
     }
 
-    public String generate(ASTNode root) {
+    public void generate(ASTNode root) {
         if (!"Program".equals(root.getNodeType())) {
             throw new IllegalArgumentException("Root node must be of type 'Program'");
         }
 
         for (ASTNode child : root.getChildren()) {
-            if ("Class".equals(child.getNodeType())) {
+            if ("class".equals(child.getNodeType())) {
                 generateClass(child);
             }
         }
 
-        return bytecode.toString();
+        //return bytecode.toString();
     }
 
     private void generateClass(ASTNode classNode) {
-        String className = classNode.getNodeName();
+        className = classNode.getNodeName();
         bytecode.append(".class public ").append(className).append("\n");
         bytecode.append(".super java/lang/Object\n\n");
 
         for (ASTNode child : classNode.getChildren()) {
             switch (child.getNodeType()) {
-                case "Declaration":
+                case "declaration":
                     generateField(className, child);
                     break;
-                case "Constructor":
+                case "constructor":
                     generateConstructor(className, child);
                     break;
-                case "Method":
+                case "method":
                     generateMethod(className, child);
                     break;
+                case "extends":
+                    continue;
                 default:
                     throw new UnsupportedOperationException("Unknown class element: " + child.getNodeType());
             }
+        }
+
+        try (FileOutputStream fos = new FileOutputStream("/Users/demanzverev/IdeaProjects/compiler-construction/src/main/java/examples/" + className + ".j")) {
+            fos.write(bytecode.toString().getBytes());
+            log.info("Generated class " + className);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     private void generateField(String className, ASTNode varNode) {
         String fieldName = varNode.getNodeName();
         String fieldType = mapType(varNode.getNodeTypeInfo());
-        bytecode.append(".field private ").append(fieldName).append(" ").append(fieldType).append("\n");
+        if (fieldType == null) {
+            bytecode.append("new ").append(className).append("\n");
+            bytecode.append("dup\n");
+            if (varNode.getChildren().get(0).getNodeType().equals("ConstructorCall")) {
+                bytecode.append("iconst_1\n").append("bipush 10\n")
+                        .append("ldc \"text\"\n")
+                        .append("invokespecial ").append(className).append("/<init>(ILjava/lang/String;)V\n");
+            }
+        } else {
+            bytecode.append(".field private ").append(fieldName).append(" ").append(fieldType).append("\n");
+        }
     }
 
     private void generateConstructor(String className, ASTNode constructorNode) {
         bytecode.append("\n.method public <init>(");
 
+        Map types = new HashMap<String, String>();
         for (ASTNode arg : constructorNode.getChildren()) {
-            if ("Argument".equals(arg.getNodeType())) {
+            if ("argument".equals(arg.getNodeType())) {
                 bytecode.append(mapType(arg.getNodeTypeInfo()));
+                types.put(arg.getNodeName(), mapType(arg.getNodeTypeInfo()));
             }
         }
 
@@ -68,12 +101,12 @@ public class BytecodeGenerator {
 
         int localIndex = 1;
         for (ASTNode child : constructorNode.getChildren()) {
-            if ("Assignment".equals(child.getNodeType())) {
+            if ("assignment".equals(child.getNodeType())) {
                 ASTNode targetNode = child.getChildren().get(0);
                 if ("this".equals(child.getNodeTypeInfo())) {
                     bytecode.append("    aload_0\n");
                     bytecode.append("    iload_").append(localIndex++).append("\n");
-                    bytecode.append("    putfield ").append(className).append("/").append(targetNode.getNodeName()).append(" I\n");
+                    bytecode.append("    putfield ").append(className).append("/").append(targetNode.getNodeName()).append(" ").append(types.get(targetNode.getNodeName())).append("\n");
                 }
             }
         }
@@ -85,7 +118,10 @@ public class BytecodeGenerator {
     private void generateMethod(String className, ASTNode methodNode) {
         String returnType = "V";
         String methodName = methodNode.getNodeName();
-        bytecode.append("\n.method public ").append(methodName).append("(");
+        if (methodName.equals("main"))
+            bytecode.append(".method public static main([Ljava/lang/String;)V\n");
+        else
+            bytecode.append("\n.method public ").append(methodName).append("(");
 
         for (ASTNode arg : methodNode.getChildren()) {
             if ("Argument".equals(arg.getNodeType())) {
@@ -97,13 +133,14 @@ public class BytecodeGenerator {
                 returnType = mapType(child.getNodeName());
             }
         }
-        bytecode.append(")").append(returnType).append("\n");
+        if (!methodName.equals("main"))
+            bytecode.append(")").append(returnType).append("\n");
         bytecode.append("    .limit stack 3\n");
         bytecode.append("    .limit locals 3\n");
 
         for (ASTNode child : methodNode.getChildren()) {
             switch (child.getNodeType()) {
-                case "Assignment":
+                case "assignment":
                     generateAssignment(child, className);
                     break;
                 case "MethodCall":
@@ -115,6 +152,15 @@ public class BytecodeGenerator {
                     break;
                 case "ReturnStatement":
                     generateReturnStatement(child, returnType);
+                    break;
+                case "IfStatement":
+                    generateIfStatement(child);
+                    break;
+                case "WhileStatement":
+                    generateWhileStatement(child);
+                    break;
+                case "declaration":
+                    generateField(className, child);
                     break;
                 default:
                     throw new UnsupportedOperationException("Unknown method element: " + child.getNodeType());
@@ -147,7 +193,7 @@ public class BytecodeGenerator {
         ASTNode targetNode = assignNode.getChildren().get(0);
 
         if (assignNode.getChildren().size() == 1) {
-            if ("Identifier".equals(targetNode.getNodeType()) && "this".equals(assignNode.getNodeTypeInfo())) {
+            if ("identifier".equals(targetNode.getNodeType()) && "this".equals(assignNode.getNodeTypeInfo())) {
                 String fieldName = targetNode.getNodeName();
 
                 bytecode.append("    aload_0\n");
@@ -160,7 +206,7 @@ public class BytecodeGenerator {
         else if (assignNode.getChildren().size() == 2) {
             ASTNode expressionNode = assignNode.getChildren().get(1);
 
-            if ("Identifier".equals(targetNode.getNodeType())) {
+            if ("identifier".equals(targetNode.getNodeType())) {
                 String varName = targetNode.getNodeName();
 
                 generateExpression(expressionNode);
@@ -185,16 +231,18 @@ public class BytecodeGenerator {
     private void generateMethodCall(ASTNode methodCallNode) {
         String methodName = methodCallNode.getNodeName();
 
-        bytecode.append("    aload_0\n");
+        if (methodName.equals("print")) {
 
-        for (ASTNode argumentNode : methodCallNode.getChildren()) {
-            generateExpression(argumentNode);
+            bytecode.append("getstatic java/lang/System/out Ljava/io/PrintStream;\n");
+
+            for (ASTNode argumentNode : methodCallNode.getChildren()) {
+                generateExpression(argumentNode);
+            }
+            bytecode.append("invokevirtual java/io/PrintStream/println(I)V\n");
+            return;
         }
 
-        if (methodName.equals("Plus")) {
-
-        }
-        bytecode.append("    invokevirtual ").append(methodName).append("()I\n");
+        bytecode.append("    invokevirtual ").append(className).append("/").append(methodName).append("()V\n");
     }
 
     private void generateExpression(ASTNode exprNode) {
@@ -203,8 +251,8 @@ public class BytecodeGenerator {
                 bytecode.append("    ldc ").append(exprNode.getNodeName()).append("\n");
                 break;
 
-            case "Identifier":
-                bytecode.append("    aload_").append(exprNode.getNodeName()).append("\n");
+            case "identifier":
+                bytecode.append("aload_0\n").append("getfield ").append(className).append("/").append(exprNode.getNodeName()).append(" I\n");
                 break;
 
             case "MethodCall":
@@ -216,16 +264,54 @@ public class BytecodeGenerator {
         }
     }
 
+    private void generateIfStatement(ASTNode ifStatementNode) {
+        String labelTrue = "L" + System.nanoTime();
+        String labelEnd = "L" + System.nanoTime();
+
+        ASTNode conditionNode = ifStatementNode.getChildren().get(0);
+        generateExpression(conditionNode);
+
+        bytecode.append("  ifne ").append(labelTrue).append("\n");
+        bytecode.append("  goto ").append(labelEnd).append("\n");
+
+        bytecode.append(labelTrue).append(":\n");
+        ASTNode trueBranchNode = ifStatementNode.getChildren().get(1);
+        generateExpression(trueBranchNode);
+
+        bytecode.append(labelEnd).append(":\n");
+    }
+
+    private void generateWhileStatement(ASTNode whileStatementNode) {
+        String labelStart = "L" + System.nanoTime();
+        String labelEnd = "L" + System.nanoTime();
+
+        bytecode.append(labelStart).append(":\n");
+        ASTNode conditionNode = whileStatementNode.getChildren().get(0);
+        generateExpression(conditionNode);
+
+        bytecode.append("  ifeq ").append(labelEnd).append("\n");
+
+        ASTNode bodyNode = whileStatementNode.getChildren().get(1);
+        generateExpression(bodyNode);
+
+        bytecode.append("  goto ").append(labelStart).append("\n");
+        bytecode.append(labelEnd).append(":\n");
+    }
+
     private String mapType(String type) {
         switch (type) {
             case "Integer":
                 return "I";
             case "String":
                 return "Ljava/lang/String;";
+            case "Real":
+                return "D";
+            case "Boolean":
+                return "Z";
             case "Array[Integer]":
                 return "[I";
             default:
-                throw new IllegalArgumentException("Unsupported type: " + type);
+                return null;
         }
     }
 }
